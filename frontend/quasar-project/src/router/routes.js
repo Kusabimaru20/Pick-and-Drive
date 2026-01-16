9e76a1c3f4c0ff592f6a7eb8a7bf2438fd6e8ee1
idekbr233 <pati.pilat@gmail.com>
2026-01-16 15:16:53 +0100

    Dodano od Ante, gledajte komentare u kodu

--- frontend/quasar-project/src/router/routes.js
+++ frontend/quasar-project/src/router/routes.js
@@ -27,7 +27,7 @@ const routes = [
   {
     path: '/lokacije',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [{ path: '', component: () => import('pages/LokacijaPage.vue') }],
   },
   {
     path: '/prijava',
